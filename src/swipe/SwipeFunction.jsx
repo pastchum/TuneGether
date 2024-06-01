60f06c55c7ebcb8ab36cefeb3611a1a1f58891a5
pastchum <patrick.navin.thomas123@gmail.com>
2024-06-02 00:44:31 +0800

    Addition of pull to refresh

--- src/swipe/SwipeFunction.jsx
+++ src/swipe/SwipeFunction.jsx
@@ -1,13 +1,52 @@
 import React, { useEffect, useState } from 'react';
-import { View, TouchableOpacity, FlatList} from 'react-native';
+import { View, TouchableOpacity, FlatList, RefreshControl } from 'react-native';
 import { Styles, phoneWidth } from '../../assets/Styles';
+
+//get profile rendering function
 import { renderProfile } from '../profile/RenderProfiles';
+
+//get auth context for current user
 import { useAuth } from '../authContext/Auth-Context';
+
+//get firestore
 import firestore from '@react-native-firebase/firestore'
 
 function SwipeFunction( { navigation } ) {
+    //set current profile for rendering
     const [currentProfile, setCurrentProfile] = useState([]);
+
+    //refresh state
+    const [refreshing, setRefreshing] = useState(false);
+
+    //get current user
     const { user } = useAuth();
+
+    //logic for loading data
+    const loadData = async() => {
+        setRefreshing(true);
+        try {
+            if (user) {
+                const profilesSnapshots = await firestore().collection('users').get();
+                const profiles = profilesSnapshots.docs
+                    .filter(docs => docs.data().userId !== user.uid)
+                    .map(doc => doc.data());
+                setCurrentProfile(profiles);
+            }
+        } catch (error) {
+            console.error("Error loading profiles", error);
+            throw error;
+        } finally {
+            setRefreshing(false);
+        }
+    };
+
+    const onRefresh = () => {
+        loadData();
+    };
+
+    useEffect(() => {
+        loadData();
+    }, []);
 
     //function to render profiles as a button that navigates to its own details screen
     const profileRender = ({ item }) => (
@@ -19,26 +58,6 @@ function SwipeFunction( { navigation } ) {
             </TouchableOpacity>
         </View>
     );
-
-    useEffect(() => {
-        //get all but own profile
-        const loadData = async() => {
-            try {
-                if (user) {
-                    const profilesSnapshots = await firestore().collection('users').get();
-                    const profiles = profilesSnapshots.docs
-                        .filter(docs => docs.data().userId !== user.uid)
-                        .map(doc => doc.data());
-                    setCurrentProfile(profiles);
-                }
-            } catch (error) {
-                console.error("Error loading profiles", error);
-                throw error;
-            }
-        };
-
-        loadData();
-    }, [setCurrentProfile]);
     
     /*const Divider = () => (
         <View style={{
@@ -55,6 +74,9 @@ function SwipeFunction( { navigation } ) {
                 console.log(profile);
                 return profileRender(profile);
             }} 
+            refreshControl={
+                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }
             initialNumToRender={1}
             horizontal={true} 
             pagingEnabled={true}
